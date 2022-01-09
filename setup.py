86a8b0c627e03c0fb097a5f8f2a4ab17a687d73b
stephenleo <stephen.leo87@gmail.com>
2022-01-09 11:46:52 +0800

    updating LICENCE into manifest

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ required_packages = ['bertopic==0.9.4',
 
 setuptools.setup(
     name="stripnet",
-    version="0.0.5",
+    version="0.0.6",
     author="stephenleo",
     author_email="stephen.leo87@gmail.com",
     description="STriP Net: Semantic Similarity of Scientific Papers (S3P) Network",
